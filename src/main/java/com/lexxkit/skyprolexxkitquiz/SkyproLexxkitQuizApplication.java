6b0131a01bb044092a7bcf0286e6846ca908e826
Lexxkit <lexxkit@gmail.com>
2022-07-12 11:24:21 +0100

    init

---
+++ src/main/java/com/lexxkit/skyprolexxkitquiz/SkyproLexxkitQuizApplication.java
@@ -0,0 +1,13 @@
+package com.lexxkit.skyprolexxkitquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproLexxkitQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproLexxkitQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lexxkit/skyprolexxkitquiz/SkyproLexxkitQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexxkit.skyprolexxkitquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproLexxkitQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

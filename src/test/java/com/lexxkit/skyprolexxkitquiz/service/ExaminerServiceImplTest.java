ecc26b32d59635c930b3a3e55f798b6e0e24fb3a
Lexxkit <lexxkit@gmail.com>
2022-07-13 09:17:17 +0100

    added test case with bad amount when exception is thrown;

--- src/test/java/com/lexxkit/skyprolexxkitquiz/service/ExaminerServiceImplTest.java
+++ src/test/java/com/lexxkit/skyprolexxkitquiz/service/ExaminerServiceImplTest.java
@@ -1,7 +1,10 @@
 package com.lexxkit.skyprolexxkitquiz.service;
 
+import com.lexxkit.skyprolexxkitquiz.exception.BadAmountOfQuestionsException;
+import org.assertj.core.api.Assertions;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -21,9 +24,17 @@ class ExaminerServiceImplTest {
     @InjectMocks
     private ExaminerServiceImpl out;
 
-    @BeforeEach
-    void setUp() {
-        when(questionService.getRandomQuestion())
-                .thenReturn(Lists.newArrayList(TEST_SET_SIZE_2).get(new Random().nextInt(TEST_SET_SIZE_2.size())));
+    // TODO: 13.07.2022 Probably this lines of code could be deleted or each test case should contain this line.
+//    @Before
+//    void setUp() {
+//        when(questionService.getRandomQuestion())
+//                .thenReturn(Lists.newArrayList(TEST_SET_SIZE_2).get(new Random().nextInt(TEST_SET_SIZE_2.size())));
+//    }
+
+    // TODO: 13.07.2022 Think about parametrized test with -1, 0;
+    @Test
+    void shouldThrowBadAmountOfQuestionsExceptionWhenAmountEqualsZero() {
+        Assertions.assertThatExceptionOfType(BadAmountOfQuestionsException.class)
+                .isThrownBy(() -> out.getQuestions(0));
     }
 }

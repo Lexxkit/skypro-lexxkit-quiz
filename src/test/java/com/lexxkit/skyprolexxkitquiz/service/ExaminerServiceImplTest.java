f9eafc984c4458b8e24b022dc751d3aae151be3d
Lexxkit <lexxkit@gmail.com>
2022-07-13 09:10:06 +0100

    added some basic code to ExaminerServiceImplTest;

--- src/test/java/com/lexxkit/skyprolexxkitquiz/service/ExaminerServiceImplTest.java
+++ src/test/java/com/lexxkit/skyprolexxkitquiz/service/ExaminerServiceImplTest.java
@@ -1,8 +1,29 @@
 package com.lexxkit.skyprolexxkitquiz.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Random;
+
+import static com.lexxkit.skyprolexxkitquiz.service.QuestionServiceTestConstants.TEST_SET_SIZE_2;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class ExaminerServiceImplTest {
     // TODO: 12.07.2022 Add mock for QuestionService and Inject it to ExaminerServiceImpl
+    @Mock
+    private QuestionService questionService;
 
+    @InjectMocks
+    private ExaminerServiceImpl out;
+
+    @BeforeEach
+    void setUp() {
+        when(questionService.getRandomQuestion())
+                .thenReturn(Lists.newArrayList(TEST_SET_SIZE_2).get(new Random().nextInt(TEST_SET_SIZE_2.size())));
+    }
 }

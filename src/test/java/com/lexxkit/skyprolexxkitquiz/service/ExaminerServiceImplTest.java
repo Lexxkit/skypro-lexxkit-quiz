5a38f0e0985c59f605493b92a84ea2178a442f33
Lexxkit <lexxkit@gmail.com>
2022-07-15 13:43:23 +0100

    added positive test case;

--- src/test/java/com/lexxkit/skyprolexxkitquiz/service/ExaminerServiceImplTest.java
+++ src/test/java/com/lexxkit/skyprolexxkitquiz/service/ExaminerServiceImplTest.java
@@ -1,40 +1,54 @@
 package com.lexxkit.skyprolexxkitquiz.service;
 
+import com.lexxkit.skyprolexxkitquiz.domain.Question;
 import com.lexxkit.skyprolexxkitquiz.exception.BadAmountOfQuestionsException;
-import org.assertj.core.api.Assertions;
-import org.assertj.core.util.Lists;
-import org.junit.jupiter.api.BeforeEach;
+import com.lexxkit.skyprolexxkitquiz.exception.NotEnoughQuestionsException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static com.lexxkit.skyprolexxkitquiz.service.QuestionServiceTestConstants.*;
 import static com.lexxkit.skyprolexxkitquiz.service.QuestionServiceTestConstants.TEST_SET_SIZE_2;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ExaminerServiceImplTest {
-    // TODO: 12.07.2022 Add mock for QuestionService and Inject it to ExaminerServiceImpl
     @Mock
     private QuestionService questionService;
 
     @InjectMocks
     private ExaminerServiceImpl out;
 
-    // TODO: 13.07.2022 Probably this lines of code could be deleted or each test case should contain this line.
-//    @Before
-//    void setUp() {
-//        when(questionService.getRandomQuestion())
-//                .thenReturn(Lists.newArrayList(TEST_SET_SIZE_2).get(new Random().nextInt(TEST_SET_SIZE_2.size())));
-//    }
-
     // TODO: 13.07.2022 Think about parametrized test with -1, 0;
     @Test
     void shouldThrowBadAmountOfQuestionsExceptionWhenAmountEqualsZero() {
-        Assertions.assertThatExceptionOfType(BadAmountOfQuestionsException.class)
+        assertThatExceptionOfType(BadAmountOfQuestionsException.class)
                 .isThrownBy(() -> out.getQuestions(0));
+    }
+
+    @Test
+    void shouldThrowNotEnoughQuestionsExceptionWhenAmountGreaterThanQuestionsSize() {
+        when(questionService.getAll()).thenReturn(TEST_SET_SIZE_2);
+        assertThatExceptionOfType(NotEnoughQuestionsException.class)
+                .isThrownBy(() -> out.getQuestions(TEST_SET_SIZE_2.size() + 1));
+    }
+
+    @Test
+    void shouldReturnCollectionOfRandomQuestionsWithSizeTwoWhenAmountIsTwo() {
+        when(questionService.getAll()).thenReturn(TEST_SET_SIZE_4);
+        when(questionService.getRandomQuestion())
+                .thenReturn(TEST_QUESTION_1)
+                .thenReturn(TEST_QUESTION_1)
+                .thenReturn(TEST_QUESTION_3);
+
+        Collection<Question> result = out.getQuestions(2);
+        assertThat(result).hasSize(2);
+        assertThat(result).containsExactlyInAnyOrder(TEST_QUESTION_1, TEST_QUESTION_3);
     }
 }

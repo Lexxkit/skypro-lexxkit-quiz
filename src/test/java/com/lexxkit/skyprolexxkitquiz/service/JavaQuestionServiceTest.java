f0954d7762c3fe3bc842ae354d832fd0582a2765
Lexxkit <lexxkit@gmail.com>
2022-07-22 13:21:30 +0100

    refactor JavaQuestionServiceTest and MathQuestionServiceTest with repository mocks;

--- src/test/java/com/lexxkit/skyprolexxkitquiz/service/JavaQuestionServiceTest.java
+++ src/test/java/com/lexxkit/skyprolexxkitquiz/service/JavaQuestionServiceTest.java
@@ -5,7 +5,6 @@ import com.lexxkit.skyprolexxkitquiz.exception.JavaQuestionsIsEmptyException;
 import com.lexxkit.skyprolexxkitquiz.exception.QuestionAlreadyAddedException;
 import com.lexxkit.skyprolexxkitquiz.exception.QuestionNotFoundException;
 import com.lexxkit.skyprolexxkitquiz.repository.QuestionRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,6 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Set;
 
 import static com.lexxkit.skyprolexxkitquiz.QuestionServiceTestConstants.*;
 import static org.assertj.core.api.Assertions.*;
@@ -37,11 +37,23 @@ class JavaQuestionServiceTest {
     }
 
     @Test
+    void shouldThrowQuestionAlreadyAddedExceptionWhenAddSameQuestionWithFields() {
+        when(repository.add(TEST_QUESTION_1)).thenThrow(QuestionAlreadyAddedException.class);
+        assertThatThrownBy(() -> out.add(TEST_QUESTION_1.getQuestion(), TEST_QUESTION_1.getAnswer())).isInstanceOf(QuestionAlreadyAddedException.class);
+    }
+
+    @Test
     void shouldAddQuestionNotInSetAndReturnIt() {
         when(repository.add(NEW_TEST_QUESTION)).thenReturn(NEW_TEST_QUESTION);
 
         Question result = out.add(NEW_TEST_QUESTION);
         assertThat(result).isEqualTo(NEW_TEST_QUESTION);
+    }
+
+    @Test
+    void shouldThrowQuestionAlreadyAddedExceptionWhenAddSameQuestion() {
+        when(repository.add(TEST_QUESTION_1)).thenThrow(QuestionAlreadyAddedException.class);
+        assertThatThrownBy(() -> out.add(TEST_QUESTION_1)).isInstanceOf(QuestionAlreadyAddedException.class);
     }
 
     @Test
@@ -53,13 +65,20 @@ class JavaQuestionServiceTest {
     }
 
     @Test
-    void shouldReturnUnmodifiableSetWithAllQuestions() {
+    void shouldThrowQuestionNotFoundExceptionWhenRemoveNonExistentQuestion() {
+        when(repository.remove(NON_EXISTENT_QUESTION_1)).thenThrow(QuestionNotFoundException.class);
+        assertThatExceptionOfType(QuestionNotFoundException.class).isThrownBy(() -> out.remove(NON_EXISTENT_QUESTION_1));
+    }
+
+    @Test
+    void shouldReturnSetWithAllQuestions() {
         when(repository.getAll()).thenReturn(TEST_SET_SIZE_2);
 
         Collection<Question> result = out.getAll();
 
         assertThat(result).hasSize(TEST_SET_SIZE_2.size());
         assertThat(result).containsAll(TEST_SET_SIZE_2);
+        assertThat(result).isInstanceOf(Set.class);
     }
 
     @Test
@@ -74,7 +93,7 @@ class JavaQuestionServiceTest {
 
     @Test
     void shouldThrowJavaQuestionsIsEmptyExceptionWhenSetIsEmpty() {
-        when(out.getAll()).thenReturn(Collections.emptySet());
+        when(repository.getAll()).thenReturn(Collections.emptySet());
 
         assertThatThrownBy(() -> out.getRandomQuestion()).isInstanceOf(JavaQuestionsIsEmptyException.class);
     }
